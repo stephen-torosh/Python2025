7f8b50670efeff6d75a3cbc6adf8d539d4cd15c9
Ace-CoLd-HOoDy <98554933+stephen-torosh@users.noreply.github.com>
2025-01-23 14:05:17 +0200

    Найменьший дільник i Сума 10 числ який зупиняеться як тільки побаче від'ємне число

--- main.py
+++ main.py
@@ -1,8 +1,32 @@
 # 16.01.2025
-n = int(input("Enter any number: "))
+
+#Сума всіх парних чисел
+# n = int(input("Enter any number: "))
+# sum = 0
+# while n != 0:
+#     if n % 2 == 0:
+#         sum += n % 10
+#     n //= 10
+# print(sum)
+
+
+#23.01.2025
+
+#Найменьший дільник
+# n = int(input("Enter any number: "))
+# for i in range(2, n + 1):
+#     if n % i == 0:
+#         print("Your smallest devisor is: ", i)
+#         break
+
+#Сума 10 числ який зупиняеться як тільки побаче від'ємне число
+
 sum = 0
-while n != 0:
-    if n % 2 == 0:
-        sum += n % 10
-    n //= 10
-print(sum)  
+for i in range (10):
+    number = int(input("Enter any number: "))
+    if number > 0:
+        sum += number
+        print(sum)
+    else:
+        print(sum)
+        break
